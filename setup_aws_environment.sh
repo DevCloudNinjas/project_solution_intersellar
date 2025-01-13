e2b352b0747a91b7bc5a7724b91f8b9e0f5225f9
Lionel Tchami <65927599+lioneltchami@users.noreply.github.com>
2025-01-12 19:22:25 -0700

    correct docker command

--- setup_aws_environment.sh
+++ setup_aws_environment.sh
@@ -80,7 +80,7 @@ INSTANCE_ID=$(aws ec2 run-instances \
     --user-data '#!/bin/bash
                  yum update -y
                  yum install docker -y
-                 systemctl enable start
+                 systemctl enable docker
                  systemctl start docker
                  sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
                  sudo chmod +x /usr/local/bin/docker-compose
